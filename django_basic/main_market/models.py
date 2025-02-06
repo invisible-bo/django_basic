e5b6ff75a1a7050295823ec01b2f5088c12e0adc
invisible-bo <qhrms111@gmail.com>
2025-02-06 20:33:25 +0900

    fix_datefield2

--- django_basic/main_market/models.py
+++ django_basic/main_market/models.py
@@ -6,5 +6,5 @@ class MainMarket(models.Model):
     area = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     area_num = models.CharField(max_length=50)
-    open_date = models.DateField()
+    open_date = models.DateField(auto_now_add=True)
     worker_num = models.IntegerField()
