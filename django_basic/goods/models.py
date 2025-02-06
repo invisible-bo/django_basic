1345f4758c94a3ff78a48dc6f272ef72552887c0
invisible-bo <qhrms111@gmail.com>
2025-02-06 20:27:23 +0900

    add_models

--- django_basic/goods/models.py
+++ django_basic/goods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from main_market.models import MainMarket
 
 # Create your models here.
+class Goods(models.Model):
+    goods_ID = models.CharField(max_length=300, primary_key=True)
+    goods_name = models.CharField(max_length=300)
+    goods_price = models.IntegerField()
+    goods_stock = models.IntegerField()
+    # ForeignKey (첫번째인자 외부 class, 외부 class에있는 객체가 삭제될때 연결되어있는 객체도 삭제
+    # 세번째인자:역참조에 대한 이름을 정의해줌) 
+    market_ID = models.ForeignKey(MainMarket, on_delete=models.CASCADE, related_name="goods")
+
